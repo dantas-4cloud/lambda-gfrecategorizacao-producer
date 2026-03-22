f6e8575d95e76789601639fac0f6f9e75c0c0e6a
fcd00 <fcd007@hotmail.com>
2026-03-21 21:13:30 -0300

    feat(ajuste de projeto inicializando recursos): ajuste de projeto estrutura inicial

--- infra/modules/cloudwatch/logs.tf
+++
@@ -1,8 +0,0 @@
-# ============ CloudWatch Log Group ============
-
-resource "aws_cloudwatch_log_group" "lambda_logs" {
-  name              = "/aws/lambda/${var.lambda_function_name}-${var.environment}"
-  retention_in_days = var.cloudwatch_log_retention_days
-
-  tags = var.tags
-}
--- infra/modules/dynamodb/data_sources.tf
+++
@@ -1,5 +0,0 @@
-# ============ Data Sources ============
-
-data "aws_dynamodb_table" "transactions" {
-  name = var.dynamodb_transactions_table_name
-}
